0fd7105d2641f1c2955f099ed447c2dfee6bd981
re <ostio@elesy.ru>
2018-04-03 13:26:44 +0700

    AddItem1View style

---
+++ VisitorsAccounting/VisitorsAccounting/Views/AddItem1View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisitorsAccounting.Views
+{
+    /// <summary>
+    /// Interaction logic for AddItem1View.xaml
+    /// </summary>
+    public partial class AddItem1View : Window
+    {
+        public AddItem1View()
+        {
+            InitializeComponent();
+        }
+    }
+}
